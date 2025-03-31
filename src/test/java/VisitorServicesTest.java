c95c4dde2963bff76e59801e11b5e70ef49d993b
Sandip Rajbhandari <srb.newar.yb0111@gmail.com>
2025-03-31 10:16:55 +0545

    test case of visitor

---
+++ src/test/java/VisitorServicesTest.java
@@ -0,0 +1,78 @@
+import daoInterface.VisitorsDAO;
+import model.StatusMessageModel;
+import model.Users;
+import model.Visitors;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import service.VisitorService;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+
+@ExtendWith(MockitoExtension.class)
+public class VisitorServicesTest {
+
+    @Mock
+    private VisitorsDAO visitorsDAO;
+
+    @InjectMocks
+    private VisitorService visitorService;
+
+
+    private String fullName;
+    private String reason;
+    private Users student;
+    private String relation;
+
+    @BeforeEach
+    void setup(){
+        student = new Users();
+        fullName = "test visitor";
+        reason = "test reason";
+        relation = "test relation";
+    }
+
+    @Test
+    void create_newVisitor(){
+
+        when(visitorsDAO.add(any(Visitors.class))).thenReturn(true);
+
+        StatusMessageModel result = visitorService.addVisitor(fullName,reason,student,relation);
+
+        assertTrue(result.isStatus());
+
+        assertEquals("New Visitor Added Successfully", result.getMessage());
+
+        verify(visitorsDAO, times(1)).add(argThat(visitors ->
+                visitors.getStudentId().equals(student) &&
+                visitors.getReason().equals(reason) &&
+                visitors.getRelation().equals(relation) &&
+                visitors.getFullName().equals(fullName)
+        ));
+    }
+
+    @Test
+    void create_newVisitor_failed(){
+
+        when(visitorsDAO.add(any(Visitors.class))).thenReturn(false);
+
+        StatusMessageModel result = visitorService.addVisitor(fullName,reason,student,relation);
+
+        assertFalse(result.isStatus());
+
+        assertEquals("Visitor Added Failed", result.getMessage());
+
+        verify(visitorsDAO, times(1)).add(argThat(visitors ->
+                visitors.getStudentId().equals(student) &&
+                        visitors.getReason().equals(reason) &&
+                        visitors.getRelation().equals(relation) &&
+                        visitors.getFullName().equals(fullName)
+        ));
+    }
+}
